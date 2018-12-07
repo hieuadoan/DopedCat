4ff761b3d7ed0979568a10fbeaa3ddffbc9a8478
Hieu Doan <hieu.a.doan@gmail.com>
2018-12-07 15:00:28 -0600

    Errors fixed

--- dopedcat/dope.py
+++ dopedcat/dope.py
@@ -3,24 +3,25 @@ from catkit import Gratoms
 from catkit.build import molecule 
 from catkit.gen.adsorption import Builder
 
-def doped_surface(slab,dopantString,dopantType='add',ini_surf_atoms=[]):
+def doped_surface(slabGratoms,dopantString,dopantType='add',surf_atoms=None):
     """ Add dopants to surface"""
-    slabs = []
-    surf_atoms = []
-    slab = Gratoms(slab)
+    fin_surf_atoms_list = []
     dopant = molecule(dopantString)[0]
-    ini_slab = Builder(slab) 
-    if not ini_surf_atoms:
-        ini_slab.set_surface_atoms(ini_surf_atoms)
+
+    if surf_atoms is not None:   
+        slabGratoms.set_surface_atoms(surf_atoms)
     
+    ini_surf_atoms = slabGratoms.get_surface_atoms().tolist()
+    print(ini_surf_atoms)
+    builder = Builder(slabGratoms)
     if dopantType=='add':
-        fin_slab = ini_slab.add_adsorbate(dopant, bonds=[0], index=-1)
-        fin_surf_atoms = ini_surf_atoms.append(ini_surf_atoms[-1]+1) # Dopant becomes a surface atom
-        slabs.append(fin_slab)
-        surf_atoms.append(fin_surf_atoms)
+        fin_slabs = builder.add_adsorbate(dopant, bonds=[0], index=-1)
+        for i, fin_slab in enumerate(fin_slabs):
+            fin_surf_atoms = ini_surf_atoms.append(ini_surf_atoms[-1]+1) # Dopant becomes a surface atom
+            fin_surf_atoms_list.append(fin_surf_atoms)
     elif dopantType=='replace':
         sys.exit('Working on this')
     else:
         sys.exit('Dopant type has to be either \'add\' or \'replace\'. Please choose one!')
-    
-    return(slabs, surf_atoms)
+
+    return(fin_slabs, fin_surf_atoms_list)
