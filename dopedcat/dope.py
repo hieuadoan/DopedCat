1bb73636fc8d8a2c2b09cbe45d71c1e9295d408a
Hieu Doan <hieu.a.doan@gmail.com>
2018-12-11 10:35:03 -0600

    Check and remove overlapped configs

--- dopedcat/dope.py
+++ dopedcat/dope.py
@@ -3,6 +3,19 @@ from catkit import Gratoms
 from catkit.build import molecule 
 from catkit.gen.adsorption import Builder
 from copy import copy
+from ase.utils import natural_cutoffs
+from ase.neighborlist import NeighborList
+
+def OverlappedAtoms(atoms,mult=0.9,skin=0.):
+    nc = natural_cutoffs(atoms,mult=mult)
+    nl = NeighborList(nc,skin=skin,self_interaction=False,bothways=False)
+    nl.update(atoms)
+    indicesList = []
+    for i, atom in enumerate(atoms):
+        indices, offset = nl.get_neighbors(i)
+        if indices.size!=0:
+            indicesList.append(indices.tolist())
+    return indicesList 
 
 def doped_surface(slabGratoms,dopantString,dopantType='add',ini_surf_atoms=None):
     """ Add dopants to surface"""
@@ -15,11 +28,19 @@ def doped_surface(slabGratoms,dopantString,dopantType='add',ini_surf_atoms=None)
     surf_atoms = slabGratoms.get_surface_atoms().tolist()
     builder = Builder(slabGratoms)
     if dopantType=='add':
+        overlappedList = []
         fin_slabs = builder.add_adsorbate(dopant, bonds=[0], index=-1)
         for i, fin_slab in enumerate(fin_slabs):
+            overlapped = OverlappedAtoms(fin_slab)
+            if overlapped:
+                overlappedList.append(i) 
             new_surf_atoms = copy(surf_atoms)
             new_surf_atoms.append(new_surf_atoms[-1]+1) # Dopant becomes a surface atom
             fin_slabs[i].set_surface_atoms(new_surf_atoms)
+        
+        for i in reversed(overlappedList):
+            del(fin_slabs[i])
+
     elif dopantType=='replace':
         sys.exit('Working on this')
     else:
@@ -29,7 +50,7 @@ def doped_surface(slabGratoms,dopantString,dopantType='add',ini_surf_atoms=None)
 
 
 """
-TODO: - Remove overlaped atoms configs
+TODO: - Remove overlapped atoms configs
       - Doped surface by replacement 
 """
 
